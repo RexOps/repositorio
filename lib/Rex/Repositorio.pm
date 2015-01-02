7b2ebc248f3037239f3355e29ef739062792b4a8
Jan <krimdomu@googlemail.com>
2015-01-02 13:31:44 +0100

    updated changelog and version

--- lib/Rex/Repositorio.pm
+++ lib/Rex/Repositorio.pm
@@ -21,7 +21,7 @@ use File::Spec;
 use File::Copy;
 use Rex::Repositorio::Repository_Factory;
 
-our $VERSION = "0.1.1";
+our $VERSION = "0.2.0";
 
 has config => ( is => 'ro' );
 has logger => ( is => 'ro' );
